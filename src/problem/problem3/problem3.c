9014f5e6ce810fe943bbe7985d27e732a58165ee
danielascherer <daniela.scherer@gmail.com>
2021-07-29 17:38:10 +0100

    applyMove

--- src/problem/problem3/problem3.c
+++ src/problem/problem3/problem3.c
@@ -485,12 +485,22 @@ struct solution *applyMove(struct solution *s, const struct move *v, const enum 
         /*
          * IMPLEMENT HERE
          */
+        s->nodes[v->node] = v->group;       
+        s->groups[v->group][s->group_sizes[v->group]] = v->node;
+        s->group_sizes[v->group] = s->group_sizes[v->group] + 1;
         i = 0;
         break;
     case REMOVE:
         /*
          * IMPLEMENT HERE
          */
+        s->group_sizes[v->group] = s->group_sizes[v->group] -1;
+        for(int x = 0; x <= s->group_sizes[v->group], x++){
+            if(s->groups[v->group][x] == v->node){
+                s->groups[v->group][x] = s->groups[v.group][s->group_sizes[v.group]];
+                break;
+            }
+        }
         i = 1;
         break;
     default:
